8a7e2f380e76790db6c10abcfc836eea45b4bba1
loco <locoz66666@gmail.com>
2021-12-28 12:34:05 +0800

    由于部分插件存在深度魔改的情况，并不仅仅存在于config、data、uc_server/data、uc_client/data中，所以添加一个目录用来处理这种情况，并在容器启动时自动进行文件的添加和覆盖操作

--- init.sh
+++ init.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 rsync -a /tmp/discuz_x/* /var/www/html
+rsync -a /tmp/discuz_x_overwirte/* /var/www/html
 if test -f "/var/www/html/config/config_global.php"; then
   rm /var/www/html/install/index.php
 fi
